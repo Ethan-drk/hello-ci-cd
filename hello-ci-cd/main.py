683d41f3cf99fbc830e62703dbb68a1d0391b4fd
Billy Bob <188494117+Ethan-drk@users.noreply.github.com>
2025-09-17 15:13:34 -0500

    test

--- hello-ci-cd/main.py
+++ hello-ci-cd/main.py
@@ -1,6 +1,15 @@
 def main():
     print("Hello from hello-ci-cd!")
 
+def add(a,b):
+    return a+b
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
---
+++ hello-ci-cd/test_main.py
@@ -0,0 +1,4 @@
+from main import add
+
+def test_main():
+    assert add(5,5) == 10
